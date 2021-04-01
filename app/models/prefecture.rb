668fdbf5850433fe7d7e989791cfbc58c466a6b7
kikuchida <kikuchida@gmail.com>
2021-04-01 19:47:00 +0900

    商品出品ページを表示できるようにする

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -4,7 +4,7 @@ class Prefecture < ActiveHash::Base
     { id: 2, name: '北海道' },
     { id: 3, name: '東京' },
     { id: 4, name: '愛知' },
-    { id: 5, name: '大阪'}
+    { id: 5, name: '大阪'},
     { id: 6, name: '沖縄'}
   ]
 
