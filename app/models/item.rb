99b223632c72eb71edd386dbe8d89a766efeac68
kikuchida <kikuchida@gmail.com>
2021-04-01 18:58:50 +0900

    ActiveHashを導入する、バリデーションを設定する

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category #カテゴリーテーブルとのアソシエーション
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :delivery_schedule
+
+  #空の投稿を保存できないようにする
+  validates :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_schedule_id, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_schedule_id, numericality: { other_than: 1 } 
+
 end
---
+++ app/models/prefecture.rb
@@ -0,0 +1,14 @@
+class Prefecture < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '北海道' },
+    { id: 3, name: '東京' },
+    { id: 4, name: '愛知' },
+    { id: 5, name: '大阪'}
+    { id: 6, name: '沖縄'}
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+  
+  end
