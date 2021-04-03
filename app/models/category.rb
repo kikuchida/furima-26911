b5e83405aad587bdb75825db250d802876c70ae8
kikuchida <kikuchida@gmail.com>
2021-04-03 18:17:42 +0900

    出品した商品の情報をテーブルに保存できるようにする

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ActiveHash::Base
     { id: 1, name: '--' },
     { id: 2, name: 'レディース' },
     { id: 3, name: 'メンズ' },
-    { id: 4, name: 'ベビー・キッズ' },
+    { id: 4, name: 'ベビー・キッズ' }
   ]
 
   include ActiveHash::Associations
