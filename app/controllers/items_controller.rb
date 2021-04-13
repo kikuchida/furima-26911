24305bf2032a13a8f0284f2e5f09e1108e7666f3
kikuchida <kikuchida@gmail.com>
2021-04-13 16:31:25 +0900

    レビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index,  only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)  # 正しく保存できるならという記述
       redirect_to action: :show
     else
@@ -43,4 +42,15 @@ class ItemsController < ApplicationController
     # params.require(:モデル名)  # 取得したい情報を指定する
     # .permit(:キー名) # 取得したいキーを指定する←itemテーブルのカラム
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
 end
